8fe57d6d6400a0bef6742632dc3648370f38460f
Mate Pocs <pocsmate1987@gmail.com>
2020-05-07 12:27:43 +0100

    data func script enhanced

---
+++ data_func.py
@@ -0,0 +1,61 @@
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convert_pdf_to_txt(path):
+    '''Convert pdf content from a file path to text
+
+    :path the file path
+    '''
+    rsrcmgr = PDFResourceManager()
+    codec = 'utf-8'
+    laparams = LAParams()
+
+    with io.StringIO() as retstr:
+        with TextConverter(rsrcmgr, retstr, codec=codec,
+                           laparams=laparams) as device:
+            with open(path, 'rb') as fp:
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+                password = ""
+                maxpages = 0
+                caching = True
+                pagenos = set()
+
+                for page in PDFPage.get_pages(fp,
+                                              pagenos,
+                                              maxpages=maxpages,
+                                              password=password,
+                                              caching=caching,
+                                              check_extractable=True):
+                    interpreter.process_page(page)
+
+                return retstr.getvalue()
+
+                
+def convert_title_to_filename(title):
+    filename = title.lower()
+    filename = filename.replace(' ', '_')
+    return filename
+
+
+ def split_to_title_and_pagenum(table_of_contents_entry):
+    title_and_pagenum = table_of_contents_entry.strip()
+    
+    title = None
+    pagenum = None
+    
+    if len(title_and_pagenum) > 0:
+        if title_and_pagenum[-1].isdigit():
+            i = -2
+            while title_and_pagenum[i].isdigit():
+                i -= 1
+
+            title = title_and_pagenum[:i].strip()
+            pagenum = int(title_and_pagenum[i:].strip())
+        
+    return title, pagenum
+    
